4395fe7085d2ae7dd28d810adc15d8d3885b2cee
Mario Marinero <mariomarinero@gmail.com>
2016-05-02 22:31:58 +0200

    Sync laptop

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 module.exports = function(){
-	var WeakMapProfiler = function(logger){
+
+	var DefaultReporter = function(logger){
 		this.logger = logger;
-		if (!this.logger) {
-			this.logger = console.log.bind(console);
+	}
+
+	DefaultReporter.prototype = {
+		init: function(){
+
 		}
+	}
+
+	var WeakMapProfiler = function(options){
+		options = options || [];
 		this.map = new WeakMap();
 		this.metaMap = new Map();
+		this.logger = options.logger || console.log.bind(console);
+		this.reporter = options.reporter || new WeakMapProfiler.DefaultReporter(logger);
+		this.reporter.init();
 	}
 
 	WeakMapProfiler.prototype = {
 		log: function(key, value) {
+			var valueRecords = this.metaMap.has(key) ? this.metaMap.get(key) : [];
+			var record = {
+				timestamp: performance.now(),
+				alreadyLive: this.map.has(key)
+			};
+			valueRecords.push(record)
+			this.metaMap.set(key, valueRecords);
 			this.map.set(key, value);
-			this.metaMap.set(key, {
-				timestamp: performance.now()
-			})
 		}
 	}
+
+	WeakMapProfiler.DefaultReporter = DefaultReporter;
 
 	return WeakMapProfiler;
 }();
